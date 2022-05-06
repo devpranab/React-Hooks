693ffdcc799ca163cc1ab94ba623a1557327a51a
pranav s <dev.pranabsk@gmail.com>
2022-05-06 16:08:42 +0530

    Using Array Destructuring

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,13 +1,25 @@
 import React, {useState} from "react";
 
 const Test = props => {
-    const testState = useState("");
-    if(testState[0] === ""){
-        testState[1]("Hello Pranav");
+    const [myName, setMyName] = useState("");
+    const [myInfo, setMyInfo] = useState("");
+
+    const changeState = () => {
+        setMyName("Pranav");
+        setMyInfo({
+            age: 30,
+            address: "kolkata, India"
+        })
+
     }
+  
     return(
         <div>
-      <h2>{testState[0]}</h2>
+      <button onClick={changeState}>Show</button>
+      <h2>{myName[0]}</h2>
+      <h2>{myName}</h2>
+      <h2>{myInfo.age} year</h2>
+      <h2>{myInfo.address}</h2>
         </div>
     )
 }
