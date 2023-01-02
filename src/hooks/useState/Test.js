aa4f7c2a47f6796f7664c546aec7ccc40a27bc98
pranav s <dev.pranabsk@gmail.com>
2023-01-02 14:21:00 +0530

    useState() array destructing

--- src/hooks/useState/Test.js
+++ src/hooks/useState/Test.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Test = () => {
-    const testState = useState("");
-    console.log(testState);//2 param value & f()
-    if(testState[0] === ""){
-        testState[1]("Hello Pranav");
-    }
-    return(
-        <div>
-        <h1>Use of useState hook:</h1>
-        <h2>{testState[0]}</h2>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0); //array destructure
+  // function
+  const handleIncrement = () => {
+    //const newCount = count + 1;
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+};
 
 export default Test;
